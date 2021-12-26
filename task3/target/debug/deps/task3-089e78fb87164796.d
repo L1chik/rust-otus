f682fe19742a2ac3762f986add5f1e7f1ca57894
Gleb Vlasov <l1chik@pop-os.localdomain>
2021-12-26 23:30:09 +0300

    task3 init

---
+++ task3/target/debug/deps/task3-089e78fb87164796.d
@@ -0,0 +1,5 @@
+/home/l1chik/Rust/rust-otus/task3/target/debug/deps/task3-089e78fb87164796.rmeta: src/main.rs
+
+/home/l1chik/Rust/rust-otus/task3/target/debug/deps/task3-089e78fb87164796.d: src/main.rs
+
+src/main.rs:
